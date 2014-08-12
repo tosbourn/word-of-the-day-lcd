498be6f1b5975f83d6d9332adf8bdc07e569a85a
Toby Osbourn <toby.osbourn@googlemail.com>
2014-08-12 11:35:57 +0100

    Fixing API key issue

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ This uses pifacecad for speaking to the Pi's LCD display, so we need:
 It also uses the wordnik API so we need to install it:
 
 `easy_install wordnik`
+
+Because of the wordnik integration we need an API key, for this version you just need to run an export with your key;
+
+```
+  export wordnik_api_key=YOUR_API_KEY
+  python3 wotd.py
+
+```
--- wotd.py
+++ wotd.py
@@ -5,16 +5,15 @@ from wordnik import *
 import pifacecad
 
 WORDNIK_URL = 'http://api.wordnik.com/v4'
-WORDNIK_API_KEY = os.environ["worknik_api_key"]
+WORDNIK_API_KEY = os.environ["wordnik_api_key"]
 
 def get_word_of_the_day():
   client = swagger.ApiClient(WORDNIK_API_KEY, WORDNIK_URL)
   words_api = WordsApi.WordsApi(client)
   return words_api.getWordOfTheDay()
 
-def display_word_of_the_day(wotd):
-  print wotd.word
-  print wotd.definitions[0].text
+def display_word_of_the_day(wotd, displayer):
+  displayer.output(wotd)
 
 class LCD:
   def __init__(self):
