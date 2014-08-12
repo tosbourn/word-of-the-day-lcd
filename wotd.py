86ad3c39582632db43600864e9207ed7c6d824e3
Toby Osbourn <toby.osbourn@googlemail.com>
2014-08-12 11:54:28 +0100

    Moving functions into the object

--- wotd.py
+++ wotd.py
@@ -7,25 +7,23 @@ import pifacecad
 WORDNIK_URL = 'http://api.wordnik.com/v4'
 WORDNIK_API_KEY = os.environ["wordnik_api_key"]
 
-def get_word_of_the_day():
-  client = swagger.ApiClient(WORDNIK_API_KEY, WORDNIK_URL)
-  words_api = WordsApi.WordsApi(client)
-  return words_api.getWordOfTheDay()
-
-def display_word_of_the_day(wotd, displayer):
-  displayer.output(wotd)
-
 class LCD:
   def __init__(self):
     self.cad = pifacecad.PiFaceCAD()
     self.lcd = self.cad.lcd
     self.lcd.backlight_on()
     self.lcd.blink_off()
-  def output(self, object):
-    self.lcd.write(object.word + "\n")
-    self.lcd.write(object.definitions[0].text)
+    get_word_of_the_day(self)
+
+  def get_word_of_the_day(self):
+    client = swagger.ApiClient(WORDNIK_API_KEY, WORDNIK_URL)
+    words_api = WordsApi.WordsApi(client)
+    self.wotd = words_api.getWordOfTheDay()
+
+  def display_word_of_the_day(self):
+    self.lcd.write(self.wotd.word + "\n")
+    self.lcd.write(self.wotd.definitions[0].text)
 
 if __name__ == "__main__":
-  wotd = get_word_of_the_day()
-  displayer = LCD()
-  display_word_of_the_day(wotd, displayer)
+  lcd = LCD()
+  lcd.display_word_of_the_day()
